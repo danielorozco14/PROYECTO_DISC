04f26b7ad21fe592ea8b3f49fe4c9e3083c00df4
danielorozco14 <00200617@uca.edu.sv>
2018-11-19 06:01:34 -0600

    init commit

---
+++ PROYECTO/chat/server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+
+var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.get('/messages/:user', (req,res) => {
+  var user = req.params.user
+  Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.post('/messages', async (req, res) => {
+  try{
+    var message = new Message(req.body);
+
+    var savedMessage = await message.save()
+      console.log('---- SAVED ----');
+
+    var censored = await Message.findOne({message:'badword'});
+      if(censored)
+        await Message.remove({_id: censored.id})
+      else
+        io.emit('---- MENSAJE:', req.body);
+      res.sendStatus(200);
+  }
+  catch (error){
+    res.sendStatus(500);
+    return console.log('---- ERROR',error);
+  }
+  finally{
+    console.log('---- MENSAJE POSTEADO ----')
+  }
+
+})
+
+io.on('connection', () =>{
+  console.log('---- A USER IS CONNECTED ----')
+})
+
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+  console.log('---- MONGODB CONNECTED ---->',err);
+})
+
+var server = http.listen(3000, () => {
+  console.log('SERVER IS RUNNING ON PORT', server.address().port);
+});
